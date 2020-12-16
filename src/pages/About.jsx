02761cccdecf677240ff3021648875f8edfc267a
tdmoore004 <tdmoore004@gmail.com>
2020-12-15 21:29:50 -0700

    Added link to resume.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                 <p className="aboutMe">Capable of designing and developing a polished and responsive website to attract clients and a creative eye-catching logo to highlight your brand.</p>
                 <p className="aboutMe">Worked my way through college, earning a Bachelor of Science in communication, with a minor in multi-disciplinary design from the University of Utah, later returning for a certificate in full-stack web development. Experienced with web development technologies such as HTML, CSS and JavaScript as well as design processes, practices and principles.</p>
                 <p className="aboutMe">My passion for, and experience with, design, digital media and web development make me a valuable asset on any design or development team.</p>
-                <p className="aboutMe">Learn more about my skills and experience <a className="resumeLink" href="/files/tanner-moore-resume.pdf">here</a>.</p>
+                <p className="aboutMe">Learn more about my skills and experience <a className="resumeLink" href="https://drive.google.com/file/d/1t6E71_lQpwCScM0X8OtvQ4-9fXy04nSI/view?usp=sharing">here</a>.</p>
 
             </section>
             
